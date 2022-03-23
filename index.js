bf987849d49099dea426c3affcf619173b50e956
Mariusz <mariuszbia@yahoo.com>
2022-03-23 00:03:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(guests, giftType) {
+    const thankYouMessages = [];
+    for (const guest of guests) {
+        const thankYouMessage = `Thank you, ${guest}, for the wonderful ${giftType} gift!`;
+        thankYouMessages.push(thankYouMessage);
+    }
+    return thankYouMessages;
+}
+
+function countDown(number) {
+    while(number>=0) {
+        console.log(number);
+        number--
+    }
+}
